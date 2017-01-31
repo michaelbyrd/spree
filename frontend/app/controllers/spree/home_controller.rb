02c55d92f42b7c905a613feb04d701f9eb420f02
Michael Byrd <mbyrd@bivarus.com>
2017-01-30 21:25:01 -0500

    change @products

--- frontend/app/controllers/spree/home_controller.rb
+++ frontend/app/controllers/spree/home_controller.rb
@@ -4,9 +4,10 @@ module Spree
     respond_to :html
 
     def index
-      @searcher = build_searcher(params.merge(include_images: true))
-      @products = @searcher.retrieve_products
-      @products = @products.includes(:possible_promotions) if @products.respond_to?(:includes)
+      # @searcher = build_searcher(params.merge(include_images: true))
+      # @products = @searcher.retrieve_products
+      # @products = @products.includes(:possible_promotions) if @products.respond_to?(:includes)
+      @products = Product.all
       @taxonomies = Spree::Taxonomy.includes(root: :children)
     end
   end
